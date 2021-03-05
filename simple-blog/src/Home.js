e4b429c2ac605faf454412c04475763269fd0901
Kayque Gonçalves <kayque.gsilva229@gmail.com>
2021-03-05 14:10:03 -0300

    add delete blog buttons

--- simple-blog/src/Home.js
+++ simple-blog/src/Home.js
@@ -7,12 +7,18 @@ const Home = () => {
     const [blogs, setBlogs] = useState([
         { title: 'Blog 01', body: 'Lorem ipsum...', author: 'Kayque', id: 1 }, 
         { title: 'Blog 02', body: 'Lorem ipsum...', author: 'Thiago', id: 2 }, 
-        { title: 'Blog 03', body: 'Lorem ipsum...', author: 'Pedro', id: 3 } 
+        { title: 'Blog 03', body: 'Lorem ipsum...', author: 'Kayque', id: 3 } 
     ])
+
+    const handleDelete = (id) =>{
+        const newBlogs = blogs.filter(blog => blog.id !== id)
+        setBlogs(newBlogs)
+    }
 
     return ( 
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs!" />
+            <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
+            <BlogList blogs={blogs.filter((blog) => blog.author === 'Kayque')} title="Kayque's blogs" />
         </div>
      )
 }
