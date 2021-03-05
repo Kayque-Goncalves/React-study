b838ceea55f9ffb694cb27c960ccb52bdfbcc9fc
Kayque Gonçalves <kayque.gsilva229@gmail.com>
2021-03-05 15:38:14 -0300

    handling errors

--- simple-blog/src/Home.js
+++ simple-blog/src/Home.js
@@ -5,19 +5,32 @@ const Home = () => {
 
     // eslint-disable-next-line no-unused-vars
     const [blogs, setBlogs] = useState(null)
+    const [isPending, setIsPending] = useState(true)
+    const [error, setError] = useState(null)
 
     useEffect(() => {
         fetch('http://localhost:8000/blogs')
             .then(res => {
+                if(!res.ok) {
+                    throw Error('Could not fetch the data for that resource.')
+                }
                 return res.json()
             })
             .then(data => {
                 setBlogs(data)
+                setIsPending(false)
+                setError(null)
+            })
+            .catch(err => {
+                setIsPending(false)
+                setError(err.message)
             })
     }, [])
 
     return ( 
         <div className="home">
+            { error && <div>{ error }</div> }
+            {isPending && <div> Loading... </div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs!"/>}
         </div>
      )
