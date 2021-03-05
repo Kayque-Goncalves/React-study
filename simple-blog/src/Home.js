95f4ce22d2e24bae34768778813327e81f5b54e7
Kayque Gonçalves <kayque.gsilva229@gmail.com>
2021-03-05 13:57:11 -0300

    make blog list a reusable component

--- simple-blog/src/Home.js
+++ simple-blog/src/Home.js
@@ -1,29 +1,18 @@
 import { useState } from 'react'
+import BlogList from './BlogList'
 
 const Home = () => {
-    // const [name, setName] = useState('Thiago')
-
-    // const HandleClick = () => {
-    //     setName('Kayque')
-    // }
 
     // eslint-disable-next-line no-unused-vars
     const [blogs, setBlogs] = useState([
         { title: 'Blog 01', body: 'Lorem ipsum...', author: 'Kayque', id: 1 }, 
-        { title: 'Blog 02', body: 'Lorem ipsum...', author: 'Thiago', id: 1 }, 
+        { title: 'Blog 02', body: 'Lorem ipsum...', author: 'Thiago', id: 2 }, 
         { title: 'Blog 03', body: 'Lorem ipsum...', author: 'Pedro', id: 3 } 
     ])
 
     return ( 
         <div className="home">
-            {blogs.map((blog) => (
-                <div className='blog-preview' key={ blog.id }>
-                    <h2>{ blog.title }</h2>
-                    <p>Written by { blog.author }</p>
-                </div>
-            ))}
-            {/* <p>{ name }</p> */}
-            {/* <button onClick={ HandleClick }>Click me</button> */}
+            <BlogList blogs={blogs} title="All Blogs!" />
         </div>
      )
 }
