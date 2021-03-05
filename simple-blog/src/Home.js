550e4a6ba598fe4509e0f535c68a55349766dd5c
Kayque Gonçalves <kayque.gsilva229@gmail.com>
2021-03-05 16:30:23 -0300

    add react-router-dom dependencies

--- simple-blog/src/Home.js
+++ simple-blog/src/Home.js
@@ -1,39 +1,17 @@
-import { useState, useEffect } from 'react'
 import BlogList from './BlogList'
+import useFetch from './useFetch'
 
 const Home = () => {
 
-    // eslint-disable-next-line no-unused-vars
-    const [blogs, setBlogs] = useState(null)
-    const [isPending, setIsPending] = useState(true)
-    const [error, setError] = useState(null)
-
-    useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-            .then(res => {
-                if(!res.ok) {
-                    throw Error('Could not fetch the data for that resource.')
-                }
-                return res.json()
-            })
-            .then(data => {
-                setBlogs(data)
-                setIsPending(false)
-                setError(null)
-            })
-            .catch(err => {
-                setIsPending(false)
-                setError(err.message)
-            })
-    }, [])
+    const  { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs')
 
     return ( 
         <div className="home">
             { error && <div>{ error }</div> }
-            {isPending && <div> Loading... </div>}
-            {blogs && <BlogList blogs={blogs} title="All Blogs!"/>}
+            { isPending && <div> Loading... </div> }
+            { blogs && <BlogList blogs={blogs} title="All Blogs!"/> }
         </div>
-     )
+    )
 }
 
 export default Home
